50b985e6603a02dd23cb989b2a85cb4ebf4a9b7b
dadaynamo <66999177+dadaynamo@users.noreply.github.com>
2022-04-17 21:42:07 +0200

    Update script.ts

--- script.ts
+++ script.ts
@@ -3,11 +3,11 @@
 //array archi edges
 
 //tipo formato da sole stringhe e numeri
-type StNu= string | number;
+type StNu = string | number;
 //definizione tipo oggetto nodo e oggetto arco
-type N=GNode;
-type E=GEdge;
-type Dictionary = {[key: StNu]: StNu[]}
+type N = GNode;
+type E = GEdge;
+type Dictionary = { [key: StNu]: StNu[] }
 /*
 //definizione tipo array nodi e array archi
 type arrN=[N];
@@ -15,115 +15,61 @@ type arrE=[E];
 */
 
 //class nodo
-class GNode{
+class GNode {
   //parametri
-  label:StNu;
-    
-    constructor(label:StNu){
-      this.label=label;
-    }
+  label: StNu;
+
+  constructor(label: StNu) {
+    this.label = label;
+  }
 }
 
 //class arco
-class GEdge{
-  from:N;
-  to:N;
+class GEdge {
+  from: N;
+  to: N;
 
-  constructor(from:N,to:N){
-    this.from=from;
-    this.to=to;
+  constructor(from: N, to: N) {
+    this.from = from;
+    this.to = to;
   }
-  
+
 }
 
 //class grafo
-class Graph{
-  nodes:GNode[];
-  edges:GEdge[];
-  
-  constructor(nodes:GNode[], edges:GEdge[]){
-  this.nodes=nodes;
-  this.edges=edges;
+class Graph {
+  nodes: GNode[];
+  edges: GEdge[];
+
+  constructor(nodes: GNode[], edges: GEdge[]) {
+    this.nodes = nodes;
+    this.edges = edges;
   }
 
 }
 
 
-//test fase 1
-/*
-  console.log("***********************************************");
-  
-  console.log("Test fase 1\n");
-  let	n1=new GNode(0);
-  console.log(n1);
-  let n2=new GNode("due");
-  console.log(n2);
-  let n3=new GNode("tre");
-  console.log(n3);
-  let	e1=new GEdge(n1,n2);
-  console.log(e1);
-  let e2=new GEdge(n1,n3);
-  console.log(e2);
-  let	g=new Graph([n1,n2,n3],[e1,e2]);
-  console.log(g);
-  console.log(g.edges.length);
-  console.log(g.nodes[1]===n2);
-  console.log(g.nodes[0].label);
-  console.log(g.edges[0].from===n1);
-  console.log("Fase 1 finita");
-  
-  console.log("***********************************************");
-*/
+
 
 
 //fase 2
 //capire se un grafo è connesso
 
-/*
-//stampa valore del nodo inizio di un arco
-console.log(e1.from.label);
-//stampa valore del nodo arrivo di un arco
-console.log(e1.to.label);
-//stampa numero nodi di un grafo
-console.log(g.nodes.length);
-*/
 
-/* 1 tentativo
-function connected(G:Graph){
-  var arr: StNu[]=[];
-  for(var i of G.edges){
-
-    if(nEx(arr,i.from.label)==false)
-    arr.push(i.from.label);
-    if(nEx(arr,i.to.label)==false)
-    arr.push(i.to.label);
-  }
-  if(arr.length==G.edges.length)return true;
-  else return false;
-}
-function nEx(arr:StNu[],l:StNu){
-  var b:boolean=false;
-  for(var i of arr){
-    if(i==l)  
-      b=true;
-  }
-  return b;
-}  
-*/
 
 /*controlla se un valore l 
 stringa o number è in un array arr*/
-function nEx(arr:StNu[],l:StNu){
-  var b:boolean=false;
-  for(var i of arr){
-    if(i==l)  
-      b=true;
+function nEx(arr: StNu[], l: StNu): boolean {
+  var b: boolean = false;
+  for (var i of arr) {
+    if (i == l)
+      b = true;
   }
   return b;
-} 
+}
 
 //genera lista di adiacenza di un grafo
-function genListAd(G:Graph){
+function genListAd(G: Graph): Dictionary {
   /*
   lista adiacenza
   1 | 2,3
@@ -142,17 +88,17 @@ function genListAd(G:Graph){
   arco.from  -> nodo iniziale 
   arco.to    -> nodo finale
   */
-  
-  var lAd:Dictionary={};  
-    
+
+  var lAd: Dictionary = {};
+
   //inserimento chiavi che sono i nodi
-  for(var i of G.nodes){
-    lAd[i.label]=[];
+  for (var i of G.nodes) {
+    lAd[i.label] = [];
   }
-    //console.log(lAd);
+  //console.log(lAd);
   /*inserimento nodi di uscita di ogni 
     singolo nodo*/
-  for(var a of G.edges){
+  for (var a of G.edges) {
     lAd[a.from.label].push(a.to.label);
   }
   //console.log("Lista di adiacenza grafo");
@@ -162,22 +108,22 @@ function genListAd(G:Graph){
 }
 
 //ritorna true o false se è connesso o meno il grafo
-function connected(G:Graph){
+function connected(G: Graph): boolean {
 
-let lAd=genListAd(G);  
-//console.log(lAd);
-//inizializzo sorgente
-let s:StNu=G.nodes[0].label;
-//console.log("Sorgente: "+s);
-let Bi:StNu[]=[];
-let Gr:StNu[]=[];
-let Ne:StNu[]=[];
-let Q:StNu[]=[];
-  
-//riempio array bianco senza la sorgente
-  for(let i of G.nodes){
-    if(i.label!=s){
-    Bi.push(i.label);  
+  let lAd = genListAd(G);
+  //console.log(lAd);
+  //inizializzo sorgente
+  let s: StNu = G.nodes[0].label;
+  //console.log("Sorgente: "+s);
+  let Bi: StNu[] = [];
+  let Gr: StNu[] = [];
+  let Ne: StNu[] = [];
+  let Q: StNu[] = [];
+
+  //riempio array bianco senza la sorgente
+  for (let i of G.nodes) {
+    if (i.label != s) {
+      Bi.push(i.label);
     }
   }
   //console.log("Array bianco "+Bi);
@@ -189,22 +135,22 @@ let Q:StNu[]=[];
   //inizializzo la ipotetica coda bfs
   Q.push(s);
 
-  
-  while(Q.length!=0){
-    
-    let u=Q[0];
+
+  while (Q.length != 0) {
+
+    let u = Q[0];
     /*console.log("elemento che si esaminerà       nel ciclo: "+u);*/
     Q.shift();
     //console.log(Q);
-    
-    for(let v of lAd[u]){
+
+    for (let v of lAd[u]) {
       //console.log("v"+v+"  bianco: "+Bi);
       /*nodo mai scoperto 
         ancora bianco*/
-      if(nEx(Bi,v)==true){
+      if (nEx(Bi, v) == true) {
         //tolgo dai bianchi
-        let indv=Bi.indexOf(v);
-        Bi.splice(indv,1);
+        let indv = Bi.indexOf(v);
+        Bi.splice(indv, 1);
         //aggiungo nei grigi
         Gr.push(v);
         //aggiunta in coda BFS
@@ -212,31 +158,17 @@ let Q:StNu[]=[];
       }
     }
     Ne.push(u);
-    
+
   }
   /*
   console.log("lista neri");
   console.log(Ne);
   */
-  if(Ne.length==G.nodes.length) return true;
+  if (Ne.length == G.nodes.length) return true;
   else return false;
 
 }
 
-/*
-//test fase 2
-  let	n1=new GNode(1);
-  let n2=new GNode(2);
-  let n3=new GNode(3);
-  let n4=new GNode(4);
-  let	e1=new GEdge(n1,n2);
-  let e2=new GEdge(n1,n3);
-  let e3=new GEdge(n3,n4);
-  let	g1=new Graph([n1,n2,n3,n4],[e1,e2,e3])
-  let	g2=new Graph([n1,n2,n3,n4],[e1,e3])
-  console.log(connected(g1));
-  console.log(connected(g2));
-*/
 
 
 
@@ -255,30 +187,136 @@ construttore (label,final=false){
 
 */
 class State extends GNode {
-  final:boolean;
-  constructor(label:StNu,final:boolean=false){
+  final: boolean;
+  constructor(label: StNu, final: boolean = false) {
     super(label);
-    this.final=final;
+    this.final = final;
   }
 }
 
-class Transition extends GEdge{
-from:State;
-to:State;
-evt:boolean;
-act:void;
-  
-  constructor(from:State,to:State,evt:boolean,act?:void){
-  super(from,to);
-  this.from=from;
-  this.to=to;
-  this.evt=evt;
-  this.act=act;
+type evtT = (<E>(e: E) => boolean);
+type actT = (() => void) | undefined;
+
+class Transition<E> extends GEdge {
+  from: State;
+  to: State;
+  evt: evtT;
+  act: actT;
+
+  constructor(from: State, to: State, evt: evtT, act?: actT) {
+    super(from, to);
+    this.from = from;
+    this.to = to;
+    this.evt = evt;
+    this.act = act;
+  }
+
 }
-  
+
+
+interface IAutomata<E> {
+  states: State[];
+  transitions: Transition<E>[];
+  initial: State;
+  state: State;
+  init(): void;
+  done(): boolean;
+  step(e: E): boolean;
+
 }
 
 
+class Automata<E> extends Graph implements IAutomata<E>{
+  states: State[];
+  transitions: Transition<E>[];
+  initial: State;
+  state: State;
+
+  constructor(states: State[], transitions: Transition<E>[], initial: State) {
+
+    super(states, transitions);
+    this.transitions = transitions;
+    this.states = states;
+    this.initial = initial;
+    //punto b
+    this.state = this.initial;
+    /*//punto 
+    this.state=this.state;*/
+  }
+
+  init() {
+    this.state = this.initial;
+  }
+  done() {
+    if (this.state.final == true)
+      return true;
+    else
+      return false;
+
+  }
+  step<E>(e: E) {
+    for (var i in this.transitions) {
+
+      if (this.state == this.transitions[i].from && this.transitions[i].evt(e) === true) {
+        if (this.transitions[i].act != undefined) {
+          this.transitions[i].act?.();
+        }
+        this.state = this.transitions[i].to;
+        return true;
+      }
+    }
+    return false;
+  }
+
+}
 
 
+//test 3
 
+let si = new State("iniziale");
+let s1 = new State(1);
+let s2 = new State(2);
+let sf = new State("finale", true);
+let t1 = new Transition(si, s1, (n: any) => n % 2 == 0);
+let t2 = new Transition(si, s2, (n: any) => n % 2 == 1);
+let t3 = new Transition(s1, sf, (n: any) => n < 0, () => { console.log("e") });
+let t4 = new Transition(s2, sf, (n: any) => n < 0, () => { console.log("o") });
+let t5 = new Transition(s1, s2, n => true);
+let t6 = new Transition(s2, s1, n => true);
+
+let a = new Automata([si, s1, s2, sf], [t1, t2, t3, t4, t5, t6], si);
+
+
+class Parser<E> extends Automata<E>{
+  a: string[];
+
+  constructor(states: State[], transitions: Transition<E>[], initial: State) {
+    super(states, transitions, initial);
+    this.a = [];
+
+  }
+  accept(s: string) {
+
+ 
+      //splitto la stringa
+      this.a = s.split('');
+      //init automa
+      this.init();
+    
+      for (let i = 0; i < this.a.length; i++) {
+        //return this.a[i];
+        var ex = this.step(this.a[i]);
+        //se una transizione non va a buon fine returno false
+        if (ex == false) {
+          return false;
+        }
+        //nella transizione intermedia se sono finale allora returno false
+        if (i < this.a.length-1 && this.state.final == true) {
+          return false;
+        }
+      }
+      return this.done();
+
+    
+  }
+}
